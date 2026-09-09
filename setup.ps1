03901f2b4365778ec7b1ef08d236cf547e8d0f5c
Emil Berbakov <emberbakov@gmail.com>
2026-09-09 16:25:12 -0400

    updates

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,11 @@
 if ([int]$PSVersionTable.PSVersion.Major -lt 7) {
 		winget install --id Microsoft.Powershell --source winget --accept-package-agreements --accept-source-agreements
 	}
-$config = '%userprofile%\.config'
+$h = $env:userprofile
+$config = "$env:userprofile\.config"
 $wezterm = "$config\wezterm"
 [System.Environment]::SetEnvironmentVariable('WEZTERM_CONFIG_DIR', "$wezterm", [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('WEZTERM_CONFIG_FILE', "$wezterm\wezterm.lua", [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('GLAZEWM_CONFIG_PATH', "$config\glazewm\config.yaml", [System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('HOME', '%userprofile%', [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('HOME', "$h", [System.EnvironmentVariableTarget]::User)
 
